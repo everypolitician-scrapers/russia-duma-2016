9f81e6702a687b8a06e8754c528a0502fcc8f9ec
Tony Bowden <tony@tmtm.com>
2016-10-13 08:44:09 +0100

    Add more fields from List page

--- lib/all_members_page.rb
+++ lib/all_members_page.rb
@@ -6,8 +6,11 @@ class AllMembersPage < Page
     noko.xpath('id("lists_list_elements_35")//tr[td]').map do |tr|
       tds = tr.css('td')
       {
-        name: tds[1].text.tidy,
-        url:  absolute_url(tds[1].css('a/@href').text),
+        name:    tds[1].text.tidy,
+        faction: tds[2].text.sub('Фракция ', '').tidy,
+        area:    tds[4].text.tidy,
+        image:   tds[0].css('img/@src').text,
+        source:  absolute_url(tds[1].css('a/@href').text).to_s,
       }
     end
   end
