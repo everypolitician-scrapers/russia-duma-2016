db6e7268905671cea5223007af3b35c2afd60487
Tony Bowden <tony@tmtm.com>
2016-10-13 09:10:55 +0100

    Add area_id, where applicable

--- lib/member_page.rb
+++ lib/member_page.rb
@@ -15,6 +15,10 @@ class MemberPage < Page
     date_from(noko.xpath('//p[contains(.,"Дата начала полномочий")]').text)
   end
 
+  field :area_id do
+    area_id_from(noko.xpath('//p[contains(.,"избирательного округа")]').text)
+  end
+
   private
 
   MONTHS = %w(_ января февраля марта апреля мая июня июля августа
@@ -24,5 +28,10 @@ class MemberPage < Page
     return unless md = str.to_s.match(/(\d{1,2}) (.*?) (\d{4})/)
     '%d-%02d-%02d' %
       [md.captures[2], MONTHS.find_index(md.captures[1]), md.captures[0]]
+  end
+
+  def area_id_from(str)
+    return if str.to_s.empty?
+    str[/избирательного округа\s+(\d+)/, 1]
   end
 end
